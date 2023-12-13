319bbf65b8df89aab7a3208bc3c36343f66a8d9a
guiker <ld0555889@gmail.com>
2023-12-12 20:25:02 -0800

    sim

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -3,6 +3,7 @@ var enableButton=document.getElementById("enable");
 var purzeButton=document.getElementById("purze")
 var valvePositioning = document.querySelector("#flow-rate-slider")
 var svg=document.getElementById("Layer_1");
+var weightSelection = document.getElementById("weightSelect")
 var count=0
 
 
@@ -14,8 +15,19 @@ var w4=document.getElementById("Water_4_0000001822132649291457847000001447380814
 var w5=document.getElementById("Water_5_00000121246892066337760400000013494994064657416380_")
 var w6=document.getElementById("Water_6_00000168117311549382392820000000397660189640725679_")
 var w7=document.getElementById("Water_8_00000181053212070445114170000008715850457176008092_")
+var w8 = document.getElementById("Water_7")
+var w9 = document.getElementById("Water_8")
 
+var drops = document.getElementById("Drops")
+var sW1=document.getElementById("Spear-Water")
+var sW2=document.getElementById("Main_Jet")
+var turbine = document.getElementById("turbine")
 
+var rpmText = document.getElementById("rpm-text")
+var w1Text = document.getElementById("w1-text")
+var w2Text = document.getElementById("w2-text")
+var qText = document.getElementById("q-text")
+var headText = document.getElementById("head-text")
 
 // power button
 function power(){
@@ -25,6 +37,7 @@ function power(){
         enableButton.textContent = "POWER OFF"
         count=1
         waterFlow1()
+
     }else{
         enableButton.style.backgroundColor = "#ca2222"
         document.getElementById("steps").innerHTML = "Step1: Turn Power On"
@@ -39,16 +52,16 @@ function waterFlow1(){
     // if(shouldStop){
     //     return;
     // }else{
-        if (currentwidth < 140.1) {
-            // Increment the current height by 2
-            currentwidth += 3;
-            // Set the new height to the element
-            w1.setAttribute("width", currentwidth);
-            // Call the function recursively to continue increasing the height
-            setTimeout(waterFlow1, 100); 
-        }
-            waterFlow2()
+    if (currentwidth < 140.1) {
+        // Increment the current height by 2
+        currentwidth += 3;
+        // Set the new height to the element
+        w1.setAttribute("width", currentwidth);
+        // Call the function recursively to continue increasing the height
+        setTimeout(waterFlow1, 100); 
     }
+    waterFlow2()
+}
 
 function waterFlow2(){
     w2.setAttribute("opacity", "1")
@@ -89,6 +102,7 @@ function waterFlow2(){
 
     setTimeout(function(){
         waterFlow3()
+
     },2000);
 }
 
@@ -155,14 +169,11 @@ function waterFlow4(){
 
 
 function purzeAction(){
-    console.log("gaurav ki chut")
     waterFlow4()
 }
 
 
 function waterFlow5(){
-
-
     w5.setAttribute("opacity", "1")
 
     const animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
@@ -194,11 +205,8 @@ function waterFlow5(){
     }, 1500);
     
 }
-
-
+// rotateImage()
 function waterFlow6(){
-
-
     w6.setAttribute("opacity", "1")
 
     const animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
@@ -267,9 +275,171 @@ function waterFlow7(){
     setTimeout(function(){
         // waterFlow4()
         // purzeButton.disabled = false;
-        // document.getElementById("steps").innerHTML = "Click On Flow Valve On."
+        // document.getElementById("steps").innerHTML = "Set the value of spear positoining."
+        waterFlow8()
+
 
     },3000);
 
+
+}
+
+
+
+
+function waterFlow8(){
+    sW1.setAttribute("opacity","1")
+    sW2.setAttribute("opacity","1")
+    var currentwidth = parseFloat(sW2.getAttribute("width"))
+    // // if(shouldStop){
+    // //     return;
+    // // }else{
+        if (currentwidth < 61.4) {
+            currentwidth += 5;
+            sW2.setAttribute("width", currentwidth);
+            setTimeout(waterFlow8, 200); 
+        }
+    setTimeout(function() {
+            rotateImage()
+            drops.setAttribute("opacity","1")
+            waterFlow9()
+          }, 2000)
+}
+
+function waterFlow9(){
+    w8.style.opacity="1"
+    var currentwidth = parseFloat(w8.getAttribute("width"))
+    // if(shouldStop){
+    //     return;
+    // }else{
+        if (currentwidth < 95.6) {
+            currentwidth += 3;
+            w8.setAttribute("width", currentwidth);
+            setTimeout(waterFlow9, 100); 
+        }
+        setTimeout(function() {
+            waterFlow10()
+          }, 500);
+    // }
+}
+
+function waterFlow10(){
+    var currentHeight = parseFloat(w9.getAttribute("height"))
+    // if(shouldStop){
+    //     return;
+    // }else{
+        if (currentHeight < 38.2) {
+            currentHeight += 3;
+            w9.setAttribute("height", currentHeight);
+            setTimeout(waterFlow10, 100); 
+        }
+        setTimeout(function() {
+            rpmText.textContent = "1200"
+            weightSelection.disabled = false;
+            document.getElementById("steps").innerHTML = "Select the value of brake weight."
+
+            // if(valvePositioning.value==1){
+            //     manometerText.textContent = "102 mm Hg"
+            // }
+            // if(valvePositioning.value==2){
+            //     manometerText.textContent = "146 mm Hg"
+            // }if(valvePositioning.value==3){
+            //     manometerText.textContent = "210 mm Hg"
+            // }
+
+            // purzeButton.disabled = false;
+        }, 1000);
+    // }
+    // }
+}
+
+function rotateImage() {
+    turbine.style.transformOrigin = `465px 615px`; 
+    turbine.style.animation = "rotate 5s linear infinite";
+
+    const styleSheet = document.styleSheets[0];
+styleSheet.insertRule(`
+  @keyframes rotate {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  }
+`, styleSheet.cssRules.length);
+
+}
+
+function updateValvePositioning(){
+    if(valvePositioning.value == 1){
+        headText.textContent="0.1"
+        qText.textContent = "3.251 X 10"
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }if(valvePositioning.value == 2){
+        headText.textContent="0.2"
+        qText.textContent = "4.597 X 10"
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }if(valvePositioning.value == 3){
+        headText.textContent="0.1"
+        qText.textContent = "3.251 X 10"
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }if(valvePositioning.value == 4){
+        headText.textContent="0.2"
+        qText.textContent = "4.597 X 10"
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }if(valvePositioning.value == 5){
+        headText.textContent="0.2"
+        qText.textContent = "5.597 X 10"
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }
+    valvePositioning.disabled = true;
+    rpmText.textContent = "1000"
+}
+
+// weightSelection.addEventListener("change",function(){
+//     updateBrakeWeight()
+// })
+
+function updateBrakeWeight(){
+    var selectedWeight = weightSelection.value;
+    // w1Text = selectedWeight;
+
+    if(selectedWeight == 5){
+        rpmText.textContent = "900"
+        w1Text.textContent = "5.0";
+        w2Text.textContent = "0.5";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 5kg brake weight to 1. "
+    }if(selectedWeight == 7){
+        rpmText.textContent = "800"
+        w2Text.textContent = "0.9"; 
+        w1Text.textContent = "7.0";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 7kg brake weight to 2."
+
+    }if(selectedWeight == 9){
+        rpmText.textContent = "700"
+        w2Text.textContent = "1.4";
+        w1Text.textContent = "9.0";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 9kg brake weight to 3."
+
+    }if(selectedWeight == 11){
+        rpmText.textContent = "600"
+        w2Text.textContent = "2.2";
+        w1Text.textContent = "11.0";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 11kg brake weight to 4."
+
+    }if(selectedWeight == 13){
+        rpmText.textContent = "500"
+        w2Text.textContent = "3.0";
+        w1Text.textContent = "13.0";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning for 13kg brake weight to 5."
+
+    }if(selectedWeight == 0){
+        rpmText.textContent = "0"
+        // w2Text = "3";
+        document.getElementById("steps").innerHTML = "Set the value of spear positioning greater than 0."
+    }
+    valvePositioning.disabled= false;
+    weightSelection.disabled = true;
 
 }
